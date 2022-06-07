ab8edb7a7042942c00cce1aff418df01d74fbfba
egors <egor.shobanov@gmail.com>
2022-06-07 12:58:52 +0300

    Made except for empty values

--- website/views.py
+++ website/views.py
@@ -3,32 +3,45 @@ from BACKEND import get_route_norm, oxygen_imprint, brutto, get_stations, truck_
 
 views = Blueprint('views', __name__)
 
+stations = get_stations()
+
+stations.remove("")
 
 def calculate(cargo_weight, cargo_volume, start_station, end_station):
-    if "" in (cargo_weight, cargo_volume):
-        result = None
-        flash('No routefrom', category='error')
-    else:
-        fuel_summary, length_summary = get_route_norm(start_station, end_station, brutto(cargo_weight, cargo_volume))
-        result = oxygen_imprint(fuel_summary)
-        truck_result = truck_imprint(start_station, end_station, length_summary, cargo_weight)
-        flash('Done!', category='success')
-        return [result, truck_result]
+    fuel_summary, length_summary = get_route_norm(start_station, end_station, brutto(cargo_weight, cargo_volume))
+    result = oxygen_imprint(fuel_summary)
+    truck_result = truck_imprint(start_station, end_station, length_summary, cargo_weight)
+    flash('Done!', category='success')
+    return [result, truck_result]
+
+
+    # if None in (cargo_weight, cargo_volume):
+    #     result = None
+    #     flash('No routefrom', category='error')
+    # else:
+    #     cargo_weight, cargo_volume = float(cargo_weight), float(cargo_volume)
+    #     fuel_summary, length_summary = get_route_norm(start_station, end_station, brutto(cargo_weight,
+    #                                                                                      cargo_volume))
+    #     result = oxygen_imprint(fuel_summary)
+    #     truck_result = truck_imprint(start_station, end_station, length_summary, cargo_weight)
+    #     flash('Done!', category='success')
+    #     return [result, truck_result]
 
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         if request.form['submit_button'] == 'submit1':
-            result, truck_result = calculate(float(request.form.get('cargowight')), request.form.get('cargovoluume'),
+            try:
+                result, truck_result = calculate(float(request.form.get('cargowight')), float(request.form.get('cargovoluume')),
                                              request.form.get('routefrom'), request.form.get('routeto'))
+                flash('Done!', category='success')
+            except ValueError:
+                flash('No routefrom', category='error')
+                return render_template("home.html", stations=stations)
             pie_percent = round(result/truck_result*100)
             truck_result = round(truck_result)
-            stations = get_stations()
             return render_template("home.html", result=result, pie_procent=pie_percent, truck_result=truck_result,
                                    stations=stations)
-        elif request.form['submit_button'] == 'submit2':
-            return render_template("home.html")
     else:
-        stations = get_stations()
         return render_template("home.html", stations=stations)
